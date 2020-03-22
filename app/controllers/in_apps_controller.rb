178ef7cf6c60defda328b05b26c17d1e1fd6e99b
Rodrigo Ruas <rodrigoruas2@gmail.com>
2020-03-22 12:47:49 +0000

    Add Types Controller

--- app/controllers/in_apps_controller.rb
+++ app/controllers/in_apps_controller.rb
@@ -1,7 +1,7 @@
 class InAppsController < ApplicationController
     before_action :set_in_app, only: [:show]
     def index
-        @in_apps = InApp.all
+        @in_apps = current_user.in_apps
     end
 
     def show
@@ -15,7 +15,6 @@ class InAppsController < ApplicationController
         @in_app = InApp.new(in_app_params)     
         @in_app.user = current_user
         #need the type
-        byebug
         if @in_app.save
            redirect_to in_app_path(@in_app)
         else 
